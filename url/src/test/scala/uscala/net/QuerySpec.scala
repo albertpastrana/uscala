af3870eaa50adb09a849b3efee63f71ab3a7ca99
Albert Pastrana <albert.pastrana@gmail.com>
2018-05-08 15:49:53 +0200

    Fixes asString when query has 1 param with no value
    When there was a key with no values in the query string,
    there was a implicit conversion from string to list that
    made the code compile but that messed up with the params.
    
    That way, this query string `?hello` would be translated
    into `?h&e&l&l&o`.
    
    Signed-off-by: Albert Pastrana <albert.pastrana@gmail.com>

--- url/src/test/scala/uscala/net/QuerySpec.scala
+++ url/src/test/scala/uscala/net/QuerySpec.scala
@@ -7,9 +7,12 @@ import org.specs2.specification.core.Fragment
 class QuerySpec extends Specification with ScalaCheck with TestQueries {
 
   "apply(String)" >> {
-    "when the query is null" >> {
-      "should return an empty Query" >> {
+    "should return an empty Query" >> {
+      "when the query is null" >> {
         Query(null) must beASuccessfulTry.which(_.hasQuery must_=== false)
+      }
+      "when the query is empty" >> {
+        Query("") must beASuccessfulTry.which(_.hasQuery must_=== false)
       }
     }
 
@@ -24,6 +27,18 @@ class QuerySpec extends Specification with ScalaCheck with TestQueries {
     }
   }
 
+  "asString" >> {
+    "should return empty string if it's an empty query" >> {
+      Query("").map(_.asString) must beASuccessfulTry("")
+    }
+    "should add the ? at the beginning of the query" >> {
+      Query("key=value").map(_.asString) must beASuccessfulTry("?key=value")
+    }
+    "should return only the key if there is no value" >> {
+      Query("key=").map(_.asString) must beASuccessfulTry("?key")
+    }
+  }
+
   "Empty" >> {
     "hasQuery should always return false" >> {
       Empty.hasQuery must_== false
@@ -33,6 +48,9 @@ class QuerySpec extends Specification with ScalaCheck with TestQueries {
     }
     "getOrElse should always return the default value" >> {
       Empty.getOrElse("a", List("b")) must_=== List("b")
+    }
+    "asString should return an empty string" >> {
+      Empty.asString must_=== ""
     }
   }
 
@@ -62,15 +80,15 @@ class QuerySpec extends Specification with ScalaCheck with TestQueries {
 }
 
 trait TestQueries {
-  val testQueries = List(
-    "" -> Map.empty,
+  val testQueries: List[(String, Map[String, List[String]])] = List(
     "q" -> Map("q" -> Nil),
     "q=" -> Map("q" -> Nil),
     "k:ey=a=a" -> Map("k:ey" -> List("a=a")),
     "k:ey=a?a" -> Map("k:ey" -> List("a?a")),
     "k=a&k=b&k=c" -> Map("k" -> List("a", "b", "c")),
     "foo=bar&key=value" -> Map("foo" -> List("bar"), "key" -> List("value")),
-    "foo=bar&encoded=%2F-(4)+%26+a" -> Map("foo" -> List("bar"), "encoded" -> List("/-(4) & a"))
+    "foo=bar&encoded=%2F-(4)+%26+a" -> Map("foo" -> List("bar"), "encoded" -> List("/-(4) & a")),
+    "%3Cmy_tag_2f0e5c3f94df95106fbdd455f9abb89640/%3E=" -> Map("%3Cmy_tag_2f0e5c3f94df95106fbdd455f9abb89640/%3E" -> Nil)
   )
 
 }
